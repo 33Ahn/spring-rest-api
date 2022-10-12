dc8abf493f7303d161d9a80a80f647942d348cc4
Ahn Shields <ahnshields@gmail.com>
2022-10-11 21:19:59 -0700

    Create spring project, configure Mysql database

---
+++ spring-ema/spring-ema/src/main/java/com/present/springema/SpringEmaApplication.java
@@ -0,0 +1,13 @@
+package com.present.springema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmaApplication.class, args);
+	}
+
+}
---
+++ spring-ema/spring-ema/src/test/java/com/present/springema/SpringEmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.present.springema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
