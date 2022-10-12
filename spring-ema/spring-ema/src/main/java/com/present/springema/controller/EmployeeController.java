3be92977fcc9692b7a6efc09b5484a6f26906cac
Ahn Shields <ahnshields@gmail.com>
2022-10-12 01:13:11 -0700

    Create update employee REST API

--- spring-ema/spring-ema/src/main/java/com/present/springema/controller/EmployeeController.java
+++ spring-ema/spring-ema/src/main/java/com/present/springema/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,7 +56,24 @@ public class EmployeeController {
 	@PostMapping
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);		
-	}			
+	}	
+	
+	// build update employee REST API
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id, @RequestBody Employee employeeInfo) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id: " + id));
+		
+		employee.setFirstName(employeeInfo.getFirstName());
+		employee.setLastName(employeeInfo.getLastName());
+		employee.setEmail(employeeInfo.getEmail());
+		
+		Employee updatedEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);		
+	
+
+	}
 			
 }
 	
