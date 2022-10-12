35dc3946a1eee3955445a2f84f20f46d88734d56
Ahn Shields <ahnshields@gmail.com>
2022-10-12 00:20:23 -0700

    Create add employee REST API

--- spring-ema/spring-ema/src/main/java/com/present/springema/controller/EmployeeController.java
+++ spring-ema/spring-ema/src/main/java/com/present/springema/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,11 +49,15 @@ public class EmployeeController {
 //		Employee employee = employeeRepository.findById(id)
 //				.orElseThrow(()-> new ResourceNotFoundException("Employee does not exist with id: " + id));
 //		return ResponseEntity.ok(employee);
-
-					
 	
-		
-	}
+	// build add employee REST API
+	
+	@PostMapping
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);		
+	}			
+			
+}
 	
 	
 
